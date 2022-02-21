adc9e1030e9c2ee0ff2744f4d04dfe683426d44e
jame <breakfastblast@gmail.com>
2022-02-20 20:46:53 -0500

    Sorta finish adding job history

--- src/components/form/JobHistory.tsx
+++ src/components/form/JobHistory.tsx
@@ -1,8 +1,13 @@
 import React, { FunctionComponent, useState } from "react";
-import { CV, emptyCV, ExperienceEntryType, JobHistoryType } from "../utils/CV";
+import { CV, emptyCV, ExperienceEntryType } from "../utils/CV";
 import dateFormat from "dateformat";
 import uniqid from "uniqid";
 
+interface EntryDataType {
+	newEntry: ExperienceEntryType;
+	handleInputChange: Function;
+	removeEntry: Function;
+}
 interface JobHistoryProps {
 	cv: CV;
 	updateCV: (newCV: CV) => void;
@@ -11,16 +16,14 @@ interface JobHistoryProps {
 const JobHistory: FunctionComponent<JobHistoryProps> = (
 	props: JobHistoryProps
 ) => {
-	const [state, setState] = useState({
-		experience: [] as ExperienceEntryType[],
-	});
-	const [entries, setEntriesState] = useState([] as JSX.Element[]);
+	const [state, setState] = useState([] as ExperienceEntryType[]);
+	const [entries, setEntriesState] = useState([] as EntryDataType[]);
 
-	function handleChange(newState: JobHistoryType): void {
+	function handleChange(newState: ExperienceEntryType[]): void {
 		setState(newState);
 
 		let newCV = Object.assign({}, props.cv) as CV;
-		newCV.jobHistory = newState;
+		newCV.jobHistory = { experience: newState };
 		props.updateCV(newCV);
 	}
 
@@ -34,6 +37,7 @@ const JobHistory: FunctionComponent<JobHistoryProps> = (
 
 	function newExperienceEntry() {
 		let newEntry = {
+			entryId: uniqid(),
 			companyName: "",
 			startDate: new Date(),
 			endDate: new Date(),
@@ -43,94 +47,106 @@ const JobHistory: FunctionComponent<JobHistoryProps> = (
 			description2: "",
 			description3: "",
 		} as ExperienceEntryType;
-		const newState = Object.assign({}, state);
-		newState.experience.concat(newEntry);
-		handleChange(newState);
+		let newState = state.concat(newEntry);
+		setState(newState);
 
-		const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+		const handleInputChange = (
+			event: React.FormEvent<HTMLInputElement>,
+			index: number
+		) => {
 			const { name, value } = event.currentTarget;
 			//const entryProperty = name as keyof ExperienceEntryType;
 
-			let newState = Object.assign({}, newEntry) as ExperienceEntryType;
-			newState[name] = value;
+			newEntry[name] = value;
+			const updatedState = [...state];
+			updatedState[index] = newEntry;
 
-			handleChange(state);
+			handleChange(updatedState);
 		};
 
 		const removeEntry = () => {
 			console.log("todo");
 		};
 
-		let pushed = entries.concat(
-			<section>
-				<input
-					type="text"
-					name="companyName"
-					onChange={handleInputChange}
-					placeholder="Company name"
-					value={"aaa"}
-				/>
-				<input
-					type="date"
-					name="startDate"
-					onChange={handleInputChange}
-					placeholder="Start date"
-					value={dateFormat(newEntry.endDate, "yyyy-mm-dd")}
-				/>
-				<input
-					type="date"
-					name="endDate"
-					onChange={handleInputChange}
-					placeholder="End date"
-					value={dateFormat(newEntry.endDate, "yyyy-mm-dd")}
-				/>
-				<input
-					type="text"
-					name="title"
-					onChange={handleInputChange}
-					placeholder="Title"
-					value={newEntry.title}
-				/>
-				<input
-					type="text"
-					name="location"
-					onChange={handleInputChange}
-					placeholder="Location"
-					value={newEntry.location}
-				/>
-				<input
-					type="text"
-					name="description1"
-					onChange={handleInputChange}
-					placeholder={emptyCV.jobHistory.experience[1].description1}
-					value={newEntry.description1}
-				/>
-				<input
-					type="text"
-					name="description2"
-					onChange={handleInputChange}
-					placeholder={emptyCV.jobHistory.experience[1].description2}
-					value={newEntry.description2}
-				/>
-				<input
-					type="text"
-					name="description3"
-					onChange={handleInputChange}
-					placeholder={emptyCV.jobHistory.experience[1].description3}
-					value={newEntry.description3}
-				/>
-				<button className="add" onClick={removeEntry}>
-					-
-				</button>
-			</section>
-		);
+		let pushed = entries.concat({
+			newEntry: newEntry,
+			handleInputChange: handleInputChange,
+			removeEntry: removeEntry,
+		});
 		setEntriesState(pushed);
 	}
 
 	return (
 		<section>
-			{entries.map((section) => {
-				return <div key={uniqid()}>{section}</div>;
+			{entries.map((entryData) => {
+				const index = state.findIndex(
+					(entry) => entry.entryId === entryData.newEntry.entryId
+				);
+				return (
+					<div key={uniqid()}>
+						<section>
+							<input
+								type="text"
+								name="companyName"
+								onChange={(e) => entryData.handleInputChange(e, index)}
+								placeholder="Company name"
+								value={state[index].companyName}
+							/>
+							<input
+								type="date"
+								name="startDate"
+								onChange={(e) => entryData.handleInputChange(e, index)}
+								placeholder="Start date"
+								value={dateFormat(state[index].endDate, "yyyy-mm-dd")}
+							/>
+							<input
+								type="date"
+								name="endDate"
+								onChange={(e) => entryData.handleInputChange(e, index)}
+								placeholder="End date"
+								value={dateFormat(state[index].endDate, "yyyy-mm-dd")}
+							/>
+							<input
+								type="text"
+								name="title"
+								onChange={(e) => entryData.handleInputChange(e, index)}
+								placeholder="Title"
+								value={state[index].title}
+							/>
+							<input
+								type="text"
+								name="location"
+								onChange={(e) => entryData.handleInputChange(e, index)}
+								placeholder="Location"
+								value={state[index].location}
+							/>
+							<input
+								type="text"
+								name="description1"
+								onChange={(e) => entryData.handleInputChange(e, index)}
+								placeholder={emptyCV.jobHistory.experience[1].description1}
+								value={state[index].description1}
+							/>
+							<input
+								type="text"
+								name="description2"
+								onChange={(e) => entryData.handleInputChange(e, index)}
+								placeholder={emptyCV.jobHistory.experience[1].description2}
+								value={state[index].description2}
+							/>
+							<input
+								type="text"
+								name="description3"
+								onChange={(e) => entryData.handleInputChange(e, index)}
+								placeholder={emptyCV.jobHistory.experience[1].description3}
+								value={state[index].description3}
+							/>
+							<button className="add" onClick={(e) => entryData.removeEntry()}>
+								-
+							</button>
+						</section>
+					</div>
+				);
 			})}
 			<button className="add" onClick={newExperienceEntry}>
 				+
